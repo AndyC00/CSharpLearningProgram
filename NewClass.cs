db1c878943526843622339e3ab43ae2e1e3b6734
AndyC00 <chenjinzhi96@gmail.com>
2024-07-08 18:54:44 +1200

    Initial commit

---
+++ NewClass.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace LearningProgram
+{
+    public class Person
+    {
+        public int ID {get; set; }
+        public string Name {get; set; }
+        public int Age {get; set;}
+
+        //methods
+        public void Say(string judge, string msg)
+        {
+            Console.WriteLine("hello, I am {0}", Name);
+            Console.WriteLine($"I am a {judge}");
+            Console.WriteLine(msg);
+        }
+    }
+
+    public class Student: Person
+    {
+        public string Major {get; set; }
+
+        public void Study()
+        {
+            Console.WriteLine($"{Name} is studying {Major}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using LearningProgram;
+
+class Program
+{
+    enum Days
+    {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+    }
+
+    static void Main(string[] arg)
+    {
+        int x = (int)Days.Sunday;
+        int y = (int)Days.Friday;
+        Console.WriteLine("Sun = {0}",x);
+        Console.WriteLine("Fri = {0}",y);
+
+        Person someone = new Person();
+        someone.ID = 1;
+        someone.Name = "Lily";
+        someone.Age = 20;
+
+        string msg = "Do you want to hire me?";
+        string judge = "superman";
+        someone.Say(judge, msg);
+
+        Student stu1 = new Student();
+        stu1.Name = "Micheal";
+        stu1.Age = 16;
+        stu1.ID = 2;
+        stu1.Major = "Math";
+        stu1.Study();
+
+    Console.ReadLine();
+    }
+}
